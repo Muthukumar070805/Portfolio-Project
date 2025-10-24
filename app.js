6cf3919eb1adc676b5bcf30290818341a51c1bf5
Muthu Kumar <mk407628@gmail.com>
2025-10-24 23:27:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ app.use(async (req, res, next) => {
 app.use(mainRoute);
 
 app.use((req, res, next) => {
-    res.status(404).render('404.ejs');
+    res.status(404).render('404');
 });
 app.listen(process.env.PORT || 3000);
